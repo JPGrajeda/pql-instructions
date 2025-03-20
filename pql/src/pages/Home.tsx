9eab268a389a271659aba4133c2e7410ef963928
Pablo Grajeda <grajeda.gragon@gmail.com>
2025-03-20 14:32:13 -0600

    Added necessary libraries react hook form, react table, etc...

--- pql/src/pages/Home.tsx
+++ pql/src/pages/Home.tsx
@@ -8,7 +8,21 @@ const Home = (props: propsHome) => {
 
     return(
         <React.Fragment>
-           
+           <div>
+            <form action="" method="post">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+
+                <label htmlFor="description">Description</label>
+                <input type="text" name="description" id="description" />
+            </form>
+           </div>
+
+           <div>
+                Table
+           </div>
+
+
         </React.Fragment>
     )
 } 
