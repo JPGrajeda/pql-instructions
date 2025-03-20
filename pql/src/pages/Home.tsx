aa9f47a3bbe80efd4de2a5c6bce0b1bf6f032016
Pablo Grajeda <grajeda.gragon@gmail.com>
2025-03-20 13:58:31 -0600

    Unnecessary files and default code were removed

---
+++ pql/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+interface propsHome{
+    
+}
+
+const Home = (props: propsHome) => {
+
+    return(
+        <React.Fragment>
+           
+        </React.Fragment>
+    )
+} 
+
+export default Home;
