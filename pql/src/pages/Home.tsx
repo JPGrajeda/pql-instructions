0d0213c79b1e9291cfcfd9dbbe25fbe5d4185b0b
Pablo Grajeda <grajeda.gragon@gmail.com>
2025-03-20 15:03:12 -0600

    first table with fake data

--- pql/src/pages/Home.tsx
+++ pql/src/pages/Home.tsx
@@ -1,10 +1,57 @@
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import React from "react"
 
 interface propsHome{
     
 }
 
+type Person = {
+    Name: string,
+    Age: number,
+    Position: string
+}
+
+const defaultData: Person[] = [
+    {
+      Name: 'tanner',
+      Age: 15,
+      Position: 'Chaser',
+    },
+    {
+      Name: 'tandy',
+      Age: 17,
+      Position: 'Chaser'
+    },
+    {
+      Name: 'joe',
+      Age: 19,
+      Position: 'Chaser'    },
+  ];
+
+  const columnHelper = createColumnHelper<Person>()
+
+  const columns = [
+    columnHelper.accessor('Name', {
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('Age', {
+      header: () => 'Age',
+      cell: info => info.renderValue(),
+    }),
+    columnHelper.accessor('Position', {
+        header: 'Position',
+      }),
+  ]
+
 const Home = (props: propsHome) => {
+
+    const [data, _setData] = React.useState(() => [...defaultData])
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+      })
 
     return(
         <React.Fragment>
@@ -19,7 +66,51 @@ const Home = (props: propsHome) => {
            </div>
 
            <div>
-                Table
+           <table>
+            <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                    <th key={header.id}>
+                    {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                        )}
+                    </th>
+                ))}
+                </tr>
+            ))}
+            </thead>
+            <tbody>
+            {table.getRowModel().rows.map(row => (
+                <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                    <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                ))}
+                </tr>
+            ))}
+            </tbody>
+            <tfoot>
+            {table.getFooterGroups().map(footerGroup => (
+                <tr key={footerGroup.id}>
+                {footerGroup.headers.map(header => (
+                    <th key={header.id}>
+                    {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.footer,
+                            header.getContext()
+                        )}
+                    </th>
+                ))}
+                </tr>
+            ))}
+            </tfoot>
+        </table>
            </div>
 
 
